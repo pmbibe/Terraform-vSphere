55948326fc4ff424b6ed6c5a18cfb52135a722a9
pmbibe <53257014+pmbibe@users.noreply.github.com>
2020-09-04 15:38:26 +0700

    Update vsphere.tf

--- vsphere.tf
+++ vsphere.tf
@@ -1,55 +1,119 @@
+variable "vsphere_user" {
+  type = string
+  default = "" #User vCenter
+}
+variable "vsphere_password" {
+  type = string
+  default = "" #Password vCenter
+}
+variable "vsphere_server" {
+  type = string
+  default = "" #IP vCenter
+}
+variable "network" {
+    type = string
+    default = "vlan51"
+}
+variable "datastore" {
+    type = string
+    default = "datastore1 (1)"
+}
+variable "datacenter" {
+    type = string
+    default = "k8s_datacenter"
+}
+variable "vm_temp" {
+    type = "string"
+    default = "Centos7_Temp"
+}
+variable "cluster_name" {
+    type = "string"
+    default = "k8s_cluster"
+}
+variable "server_settings" {
+    type = "map"
+    default = {
+        "server1" = {
+            IP = "192.168.51.208",
+            NETMASK = 24,
+            GATEWAY = "192.168.51.1",            
+            RAM = 1024,
+            CPU = 2,
+            DISK = 60
+        },
+        "server2" = {
+            IP = "192.168.51.209",
+            NETMASK = 24, 
+            GATEWAY = "192.168.51.1",                       
+            RAM = 1024,
+            CPU = 2,
+            DISK = 60
+        },
+        "server3" = {
+            IP = "192.168.51.210",
+            RAM = 1024,
+            NETMASK = 24,
+            GATEWAY = "192.168.51.1",           
+            CPU = 2,
+            DISK = 60
+        }
+    }
+}
+
 provider "vsphere" {
-  user           = "administrator@vsphere.local" #var.vsphere_user
-  password       = "Vega123312##" #var.vsphere_password
-  vsphere_server = "172.18.0.92" #var.vsphere_server
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
   allow_unverified_ssl = true
 }
-# resource "vsphere_folder" "k8s_folder" {
-#   path = "k8s_Datacenter"
-#   type = "datacenter"
-# }
-# resource "vsphere_datacenter" "k8s_datacenter" {
-#   name = "k8s_datacenter"
-#   folder = "/k8s_Datacenter/"
-# }
-# data "vsphere_datacenter" "k8s_datacenter" {
-#   name = "k8s_datacenter"
-# }
-# resource "vsphere_host" "h1" {
-#   hostname = "172.18.0.91"
-#   username = "root"
-#   password = "Vega123312##"
-#   license = "JU2M0-DD15J-48D89-YHAQK-0LUQ0"
-#   datacenter = data.vsphere_datacenter.k8s_datacenter.id
-# }
-data "vsphere_datacenter" "k8s_datacenter" {
-    name = "k8s_datacenter"
-}
-data "vsphere_datastore" "datastore" {
-    name = "datastore1"
-    datacenter_id = "${data.vsphere_datacenter.k8s_datacenter.id}"
+data "vsphere_datacenter" "dc" {
+    name = var.datacenter
 }
 data "vsphere_compute_cluster" "cluster" {
-  name = "k8s_resource_pool"
-  datacenter_id = "${data.vsphere_datacenter.k8s_datacenter.id}"
+    name = var.cluster_name
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
+data "vsphere_datastore" "datastore" {
+    name = var.datastore
+    datacenter_id = data.vsphere_datacenter.dc.id 
 }
 data "vsphere_network" "network" {
-  name = "VLAN181"
-  datacenter_id = ${data.vsphere_datacenter.k8s_datacenter.id}
+    name = var.network
+    datacenter_id = data.vsphere_datacenter.dc.id
 }
-resource "vsphere_virtual_machine" "vm1" {
-  name = "terraform-test"
-  datastore = "${data.vsphere_datastore.datastore.id}"
-  resource_pool_id = "${data.vsphere_compute_cluster.cluster.id}"
-  num_cpus = 2
-  memory = 1024
-  guest_id = "k8s"
-  network_interface {
-    network_id = "${data.vsphere_network.network.id}"
-  }
-  disk {
-    label = "disk0"
-    size = 40
-  }
+data "vsphere_virtual_machine" "template" {
+    name = var.vm_temp
+    datacenter_id = data.vsphere_datacenter.dc.id 
+}
+resource "vsphere_virtual_machine" "vm" {
+    for_each = var.server_settings
+    name = each.key
+    resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+    datastore_id = data.vsphere_datastore.datastore.id 
+    num_cpus = each.value.CPU
+    memory = each.value.RAM
+    guest_id = data.vsphere_virtual_machine.template.guest_id
+    network_interface {
+        network_id   = data.vsphere_network.network.id
+        adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    }
+    disk {
+        label            = "disk0"
+        size             = each.value.DISK
+    }
+    clone {
+        template_uuid = data.vsphere_virtual_machine.template.id
+        customize {
+            linux_options {
+                host_name = each.key
+                domain    = each.key
+            }
+            network_interface {
+                ipv4_address = each.value.IP
+                ipv4_netmask = each.value.NETMASK
+            }
 
+            ipv4_gateway = each.value.GATEWAY
+    }
+  }
 }
